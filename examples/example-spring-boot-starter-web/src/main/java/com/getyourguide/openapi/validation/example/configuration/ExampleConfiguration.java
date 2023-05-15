9403443c547d8114f9b8b20fd9853739e8bc362b
Patrick Boos <patrick.boos@getyourguide.com>
2023-05-15 17:39:42 +0200

    Remove unused imports

--- examples/example-spring-boot-starter-web/src/main/java/com/getyourguide/openapi/validation/example/configuration/ExampleConfiguration.java
+++ examples/example-spring-boot-starter-web/src/main/java/com/getyourguide/openapi/validation/example/configuration/ExampleConfiguration.java
@@ -1,13 +1,9 @@
 package com.getyourguide.openapi.validation.example.configuration;
 
-import com.getyourguide.openapi.validation.api.log.LogLevel;
 import com.getyourguide.openapi.validation.api.log.LoggerExtension;
 import com.getyourguide.openapi.validation.api.metrics.MetricsReporter;
-import com.getyourguide.openapi.validation.api.model.ValidatorConfiguration;
-import com.getyourguide.openapi.validation.api.model.ValidatorConfigurationBuilder;
 import com.getyourguide.openapi.validation.example.logging.ExampleLoggerExtension;
 import com.getyourguide.openapi.validation.metrics.LoggingMetricsReporter;
-import java.util.regex.Pattern;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
