6b5255ae03f3755d390f75d389f430416812db4b
Patrick Boos <patrick.boos@getyourguide.com>
2023-06-28 15:30:23 +0200

    Updated test

--- spring-boot-starter/spring-boot-starter-web/src/test/java/com/getyourguide/openapi/validation/factory/ServletMetaDataFactoryTest.java
+++ spring-boot-starter/spring-boot-starter-web/src/test/java/com/getyourguide/openapi/validation/factory/ServletMetaDataFactoryTest.java
@@ -5,11 +5,9 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class ServletMetaDataFactoryTest {
@@ -17,7 +15,7 @@ class ServletMetaDataFactoryTest {
 
     @Test
     public void testRequestMetaDataBuiltCorrectly() {
-        var request = mockRequest("/v1/test", Map.of("ids", new String[] {"1,2,3"}));
+        var request = mockRequest("/v1/test", "ids=1,2,3", Map.of("ids", new String[] {"1,2,3"}));
 
         var requestMetaData = servletMetaDataFactory.buildRequestMetaData(request);
 
@@ -28,37 +26,32 @@ class ServletMetaDataFactoryTest {
         assertEquals(
             Map.of("Accept", "application/json", "Accept-Language", "de-DE", "Content-Type", "application/json"),
             requestMetaData.getHeaders());
+        assertEquals("https://api.example.com/v1/test?ids=1,2,3", requestMetaData.getUri().toString());
     }
 
     @Test
     public void testRequestMetaDataBuiltCorrectlyWithAlreadyEncodedQueryString() {
         // Note: This seems to be the case where some clients send comma separated list with `,` being encoded
-        var request = mockRequest("/v1/test", Map.of("ids", new String[] {"1%2C2%2C3"}));
+        //       Query string has it encoded while the request.getParameterMap() has it decoded.
+        var request = mockRequest("/v1/test", "ids=1%2C2%2C3", Map.of("ids", new String[] {"1,2,3"}));
 
         var requestMetaData = servletMetaDataFactory.buildRequestMetaData(request);
 
-        assertEquals("ids=1%2C2%2C3", requestMetaData.getUri().getQuery());
+        assertEquals("ids=1,2,3", requestMetaData.getUri().getQuery());
+        assertEquals("https://api.example.com/v1/test?ids=1,2,3", requestMetaData.getUri().toString());
     }
 
     @Test
     public void testRequestMetaDataBuiltCorrectlyValueWithAndCharacter() {
-        var request = mockRequest("/v1/test", Map.of("text", new String[] {"some&more"}));
+        var request = mockRequest("/v1/test", "text=some%26more", Map.of("text", new String[] {"some&more"}));
 
         var requestMetaData = servletMetaDataFactory.buildRequestMetaData(request);
 
         assertEquals("text=some&more", requestMetaData.getUri().getQuery());
+        assertEquals("https://api.example.com/v1/test?text=some&more", requestMetaData.getUri().toString());
     }
 
-    @Test
-    public void testRequestMetaDataBuiltCorrectlyValueWithAndCharacterEncoded() {
-        var request = mockRequest("/v1/test", Map.of("text", new String[] {"some%26more"}));
-
-        var requestMetaData = servletMetaDataFactory.buildRequestMetaData(request);
-
-        assertEquals("text=some%26more", requestMetaData.getUri().getQuery());
-    }
-
-    private static HttpServletRequest mockRequest(String path, Map<String, String[]>  parameterMap) {
+    private static HttpServletRequest mockRequest(String path, String queryString, Map<String, String[]> parameterMap) {
         HttpServletRequest request = mock();
         when(request.getScheme()).thenReturn("https");
         when(request.getServerName()).thenReturn("api.example.com");
@@ -66,13 +59,7 @@ class ServletMetaDataFactoryTest {
         when(request.getMethod()).thenReturn("GET");
         when(request.getRequestURI()).thenReturn(path);
         when(request.getParameterMap()).thenReturn(parameterMap);
-        if (parameterMap != null) {
-            when(request.getQueryString())
-                .thenReturn(
-                    parameterMap.entrySet().stream()
-                        .flatMap(e -> Arrays.stream(e.getValue()).map(v -> String.format("%s=%s", e.getKey(), v)))
-                        .collect(Collectors.joining("&")));
-        }
+        when(request.getQueryString()).thenReturn(queryString);
         when(request.getHeaderNames())
             .thenReturn(Collections.enumeration(List.of("Accept", "Accept-Language", "Content-Type")));
         when(request.getHeader("Accept")).thenReturn("application/json");
