9bcd6ed0d70b7d5db3a38b40ddfdf4c107bdce18
Patrick Boos <patrick.boos@getyourguide.com>
2023-10-18 09:02:55 +0200

    Extract rule constants

--- openapi-validation-core/src/main/java/com/getyourguide/openapi/validation/core/exclusions/InternalViolationExclusions.java
+++ openapi-validation-core/src/main/java/com/getyourguide/openapi/validation/core/exclusions/InternalViolationExclusions.java
@@ -7,6 +7,11 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class InternalViolationExclusions {
+    private static final String RULE_REQUEST_OPERATION_NOT_ALLOWED = "validation.request.operation.notAllowed";
+    private static final String RULE_REQUEST_BODY_SCHEMA_ONE_OF = "validation.request.body.schema.oneOf";
+    private static final String RULE_REQUEST_PATH_MISSING = "validation.request.path.missing";
+    private static final String RULE_RESPONSE_BODY_SCHEMA_ONE_OF = "validation.response.body.schema.oneOf";
+
     private final ViolationExclusions customViolationExclusions;
 
     public boolean isExcluded(OpenApiViolation violation) {
@@ -19,15 +24,15 @@ public class InternalViolationExclusions {
 
     private static boolean oneOfMatchesMoreThanOneSchema(OpenApiViolation violation) {
         return (
-            "validation.response.body.schema.oneOf".equals(violation.getRule())
-                || "validation.request.body.schema.oneOf".equals(violation.getRule())
+            RULE_RESPONSE_BODY_SCHEMA_ONE_OF.equals(violation.getRule())
+                || RULE_REQUEST_BODY_SCHEMA_ONE_OF.equals(violation.getRule())
             )
             && violation.getMessage()
             .matches(".*Instance failed to match exactly one schema \\(matched [1-9][0-9]* out of \\d+\\).*");
     }
 
     private boolean falsePositive404(OpenApiViolation violation) {
-        return "validation.request.path.missing".equals(violation.getRule())
+        return RULE_REQUEST_PATH_MISSING.equals(violation.getRule())
             && (
                 violation.getDirection() == Direction.REQUEST
                 || (violation.getDirection() == Direction.RESPONSE && violation.getResponseStatus().orElse(0) == 404)
@@ -40,6 +45,6 @@ public class InternalViolationExclusions {
 
     private boolean falsePositive405(OpenApiViolation violation) {
         return violation.getResponseStatus().orElse(0) == 405
-            && "validation.request.operation.notAllowed".equals(violation.getRule());
+            && RULE_REQUEST_OPERATION_NOT_ALLOWED.equals(violation.getRule());
     }
 }
