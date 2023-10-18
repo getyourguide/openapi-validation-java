487d0f75fd58d067e166952f4d60221088871ae5
Patrick Boos <patrick.boos@getyourguide.com>
2023-10-18 09:08:29 +0200

    Extract rule constants to file

---
+++ openapi-validation-api/src/main/java/com/getyourguide/openapi/validation/api/Rules.java
@@ -0,0 +1,12 @@
+package com.getyourguide.openapi.validation.api;
+
+public class Rules {
+    public static class Request {
+        public static final String PATH_MISSING = "validation.request.path.missing";
+        public static final String OPERATION_NOT_ALLOWED = "validation.request.operation.notAllowed";
+        public static final String BODY_SCHEMA_ONE_OF = "validation.request.body.schema.oneOf";
+    }
+    public static class Response {
+        public static final String BODY_SCHEMA_ONE_OF = "validation.response.body.schema.oneOf";
+    }
+}
--- openapi-validation-core/src/main/java/com/getyourguide/openapi/validation/core/exclusions/InternalViolationExclusions.java
+++ openapi-validation-core/src/main/java/com/getyourguide/openapi/validation/core/exclusions/InternalViolationExclusions.java
@@ -1,5 +1,6 @@
 package com.getyourguide.openapi.validation.core.exclusions;
 
+import com.getyourguide.openapi.validation.api.Rules;
 import com.getyourguide.openapi.validation.api.exclusions.ViolationExclusions;
 import com.getyourguide.openapi.validation.api.model.Direction;
 import com.getyourguide.openapi.validation.api.model.OpenApiViolation;
@@ -7,10 +8,6 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class InternalViolationExclusions {
-    private static final String RULE_REQUEST_OPERATION_NOT_ALLOWED = "validation.request.operation.notAllowed";
-    private static final String RULE_REQUEST_BODY_SCHEMA_ONE_OF = "validation.request.body.schema.oneOf";
-    private static final String RULE_REQUEST_PATH_MISSING = "validation.request.path.missing";
-    private static final String RULE_RESPONSE_BODY_SCHEMA_ONE_OF = "validation.response.body.schema.oneOf";
 
     private final ViolationExclusions customViolationExclusions;
 
@@ -24,15 +21,15 @@ public class InternalViolationExclusions {
 
     private static boolean oneOfMatchesMoreThanOneSchema(OpenApiViolation violation) {
         return (
-            RULE_RESPONSE_BODY_SCHEMA_ONE_OF.equals(violation.getRule())
-                || RULE_REQUEST_BODY_SCHEMA_ONE_OF.equals(violation.getRule())
+            Rules.Response.BODY_SCHEMA_ONE_OF.equals(violation.getRule())
+                || Rules.Request.BODY_SCHEMA_ONE_OF.equals(violation.getRule())
             )
             && violation.getMessage()
             .matches(".*Instance failed to match exactly one schema \\(matched [1-9][0-9]* out of \\d+\\).*");
     }
 
     private boolean falsePositive404(OpenApiViolation violation) {
-        return RULE_REQUEST_PATH_MISSING.equals(violation.getRule())
+        return Rules.Request.PATH_MISSING.equals(violation.getRule())
             && (
                 violation.getDirection() == Direction.REQUEST
                 || (violation.getDirection() == Direction.RESPONSE && violation.getResponseStatus().orElse(0) == 404)
@@ -45,6 +42,6 @@ public class InternalViolationExclusions {
 
     private boolean falsePositive405(OpenApiViolation violation) {
         return violation.getResponseStatus().orElse(0) == 405
-            && RULE_REQUEST_OPERATION_NOT_ALLOWED.equals(violation.getRule());
+            && Rules.Request.OPERATION_NOT_ALLOWED.equals(violation.getRule());
     }
 }
