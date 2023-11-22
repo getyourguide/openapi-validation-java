5e77bdca7d73650e29a7e030b0a3ba1d2c57a4f4
Patrick Boos <patrick.boos@getyourguide.com>
2023-11-22 16:25:51 +0100

    fix: request body can only be read once

--- spring-boot-starter/spring-boot-starter-web/src/main/java/com/getyourguide/openapi/validation/factory/ContentCachingWrapperFactory.java
+++ spring-boot-starter/spring-boot-starter-web/src/main/java/com/getyourguide/openapi/validation/factory/ContentCachingWrapperFactory.java
@@ -1,19 +1,19 @@
 package com.getyourguide.openapi.validation.factory;
 
+import com.getyourguide.openapi.validation.filter.MultiReadHttpServletRequestWrapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import javax.annotation.Nullable;
-import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 import org.springframework.web.util.WebUtils;
 
 public class ContentCachingWrapperFactory {
-    public ContentCachingRequestWrapper buildContentCachingRequestWrapper(HttpServletRequest request) {
-        if (request instanceof ContentCachingRequestWrapper) {
-            return (ContentCachingRequestWrapper) request;
+    public MultiReadHttpServletRequestWrapper buildContentCachingRequestWrapper(HttpServletRequest request) {
+        if (request instanceof MultiReadHttpServletRequestWrapper) {
+            return (MultiReadHttpServletRequestWrapper) request;
         }
 
-        return new ContentCachingRequestWrapper(request);
+        return new MultiReadHttpServletRequestWrapper(request);
     }
 
     public ContentCachingResponseWrapper buildContentCachingResponseWrapper(HttpServletResponse response) {
@@ -26,12 +26,12 @@ public class ContentCachingWrapperFactory {
     }
 
     @Nullable
-    public ContentCachingResponseWrapper getCachingResponse(final HttpServletResponse response) {
-        return WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+    public MultiReadHttpServletRequestWrapper getCachingRequest(HttpServletRequest request) {
+        return request instanceof MultiReadHttpServletRequestWrapper ? (MultiReadHttpServletRequestWrapper) request : null;
     }
 
     @Nullable
-    public ContentCachingRequestWrapper getCachingRequest(HttpServletRequest request) {
-        return request instanceof ContentCachingRequestWrapper ? (ContentCachingRequestWrapper) request : null;
+    public ContentCachingResponseWrapper getCachingResponse(final HttpServletResponse response) {
+        return WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
     }
 }
